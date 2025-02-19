a70347e5ed1f557d969e7ebe7c40873103109b09
Mithushana-08 <shanakumar08@gmail.com>
2025-02-19 17:25:41 +0530

    SDP

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getUsers } = require('../controllers/userController');
-
-const router = express.Router();
-
-// Get all users
-router.get('/users', getUsers);
-
-module.exports = router;
